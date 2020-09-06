0ef64424a7b478eca3480cfa67e10fe94bfd8bcc
jaKub <staniojakub@gmail.com>
2020-09-06 15:29:10 +0200

    Add FAB button functionality to open Model for adding transactions

--- src/components/BottomAppBar/BottomAppBar.tsx
+++ src/components/BottomAppBar/BottomAppBar.tsx
@@ -1,24 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 /* Styles */
 import useStyles from './styles'
 /* Components */
+import AddTransactionModal from '../AddTransactionModal'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Fab from '@material-ui/core/Fab'
 
-
 const BottomAppBar: FC = () => {
   const classes = useStyles()
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <AppBar position='static' color='primary' className={classes.appBar} >
-      <Toolbar>
-        <Fab color='secondary' aria-label='add' className={classes.fabButton}>
-          +
-        </Fab>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AddTransactionModal isOpen={isModalOpen} close={() => setIsModalOpen(false)} />
+      <AppBar
+        position='absolute'
+        color='transparent'
+        classes={{
+          positionAbsolute: classes.appBar,
+        }}
+      >
+        <Toolbar classes={{ root: classes.toolbar }}>
+          <Fab color='secondary' aria-label='add' className={classes.fabButton} onClick={handleFABClicked}>
+            +
+          </Fab>
+          <NavLink to='/history' className={classes.appBarLink} activeClassName={classes.linkActive}>History</NavLink>
+        </Toolbar>
+      </AppBar>
+    </>
   )
+
+  /* Handlers */
+  function handleFABClicked(): void {
+    setIsModalOpen(true);
+  }
 }
 
 export default BottomAppBar
--- src/components/BottomAppBar/styles.ts
+++ src/components/BottomAppBar/styles.ts
@@ -1,11 +1,39 @@
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import { colors } from '../../utils/constants'
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    borderRadius: '15px 15px 0 0',
-    top: 'auto',
+    borderRadius: '50px 50px 0 0',
+    top: 'auto !important',
     bottom: 0,
+    height: '50px',
+    '& active': {
+      color: 'blue',
+    }
+  },
+  appBarLink: {
+    textDecoration: 'none',
+    fontSize: '1.1rem',
+    color: 'black',
+    marginRight: '10px',
+    borderBottom: `1px solid transparent`,
+    marginTop: '-10px',
+    transition: 'all .3s',
+    '&:hover, &:focus': {
+      color: colors.redSecondary,
+      borderBottom: `1px solid ${colors.redSecondary}`,
+    }
+  },
+  linkActive: {
+    color: colors.redSecondary,
+    borderBottom: `1px solid ${colors.redSecondary}`,
+  },
+  toolbar: {
+    borderRadius: '50px 50px 0 0',
+    background: 'transparent',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   grow: {
     flexGrow: 1,
@@ -13,7 +41,7 @@ const useStyles = makeStyles((theme) => ({
   fabButton: {
     position: 'absolute',
     zIndex: 1,
-    top: -30,
+    top: -20,
     left: 0,
     right: 0,
     margin: '0 auto',
