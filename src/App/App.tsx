9567be9ad443aa51c13727a6286bcb992f91e82a
jaKub <staniojakub@gmail.com>
2020-09-06 16:43:37 +0200

    Add functionality to save and retrieve data from localStorage

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom'
+/* Redux */
+import { useStoreActions } from '../store'
 /* Styles */
 import useStyles from './styles.'
 /* Components */
@@ -11,9 +13,23 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
 import BottomAppBar from '../components/BottomAppBar'
 
-
 const App: FC = () => {
   const classes = useStyles()
+
+  const { saveTransactions, loadTransactions } = useStoreActions(
+    (actions) => actions.transactions
+  )
+
+  /* Before refresh/closing tab save data to localStorage */
+  window.addEventListener('beforeunload', (e) => {
+    e.preventDefault()
+    saveTransactions()
+  })
+
+  useEffect(() => {
+    /* Load transactions from localStorage */
+    loadTransactions()
+  }, [loadTransactions])
 
   return (
     <>
