3673aee83371ee06c53eeefe5ab63de39ea21168
jaKub <staniojakub@gmail.com>
2020-09-06 18:11:23 +0200

    Updates

---
+++ precache-manifest.0e24661816ad105d4ba3ae3b2736af36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb74ef872bab582ab1ee79aa26936572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13bb0f994dc71fc8ff22",
+    "url": "/static/js/2.e11d7224.chunk.js"
+  },
+  {
+    "revision": "ea11c278d5345c259b089f62e805b8d5",
+    "url": "/static/js/2.e11d7224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4edcd864c64e053ba21",
+    "url": "/static/js/main.85cebefe.chunk.js"
+  },
+  {
+    "revision": "3e5e20c52f8e9bacf785",
+    "url": "/static/js/runtime-main.b6a55b0e.js"
+  }
+]);
