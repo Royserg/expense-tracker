aea4074e47436def7915588251d7f20e0022eb7a
jaKub <staniojakub@gmail.com>
2020-09-06 15:29:33 +0200

    Add styling to IncomeExpense component

--- src/components/IncomeExpenses/IncomeExpenses.tsx
+++ src/components/IncomeExpenses/IncomeExpenses.tsx
@@ -8,22 +8,38 @@ import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import Divider from '@material-ui/core/Divider'
 
-
 const IncomeExpenses: FC = () => {
-
   const classes = useStyles()
-  const { totalIncome, totalExpense } = useStoreState(state => state.transactions)
+  const { totalIncome, totalExpense } = useStoreState(
+    (state) => state.transactions
+  )
 
   return (
     <Paper elevation={2} className={classes.container}>
       <div className={classes.valueContainer}>
-        <Typography variant='h5' component='h2'>Income</Typography>
-        <p>+{totalIncome}</p>
+        <Typography
+          variant='h5'
+          component='h2'
+          className={classes.incomeHeading}
+        >
+          Income
+        </Typography>
+        <Typography variant='h6' component='h4'>
+          +{totalIncome}
+        </Typography>
       </div>
       <Divider orientation='vertical' flexItem />
       <div className={classes.valueContainer}>
-        <Typography variant='h5' component='h2'>Expense</Typography>
-        <p>{totalExpense}</p>
+        <Typography
+          variant='h5'
+          component='h2'
+          className={classes.expenseHeading}
+        >
+          Expense
+        </Typography>
+        <Typography variant='h6' component='h4'>
+          {totalExpense}
+        </Typography>
       </div>
     </Paper>
   )
--- src/components/IncomeExpenses/styles.ts
+++ src/components/IncomeExpenses/styles.ts
@@ -1,4 +1,5 @@
 import makeStyles from '@material-ui/core/styles/makeStyles'
+import { colors } from '../../utils/constants'
 
 const useStyles = makeStyles({
   container: {
@@ -8,6 +9,7 @@ const useStyles = makeStyles({
   },
   valueContainer: {
     flexGrow: 1,
+    padding: '0.55rem',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -15,8 +17,20 @@ const useStyles = makeStyles({
   },
   divider: {
     height: '50px',
-    backgroundColor: '#222'
-  }
+    backgroundColor: '#222',
+  },
+  incomeHeading: {
+    marginBottom: '0.5rem',
+    fontWeight: 'bold',
+    color: colors.greenPrimary,
+    letterSpacing: '1px',
+  },
+  expenseHeading: {
+    marginBottom: '0.5rem',
+    fontWeight: 'bold',
+    color: colors.redPrimary,
+    letterSpacing: '1px',
+  },
 })
 
 export default useStyles
